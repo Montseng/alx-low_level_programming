7929bb55fc6788c1425eccb993b6453e98d481e9
Montseng <montseng.monaheng@gmail.com>
2022-10-24 12:31:30 -0700

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - a function that takes
+ * a pointer to an int as parameter an
+ * d updates the value it prints to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
