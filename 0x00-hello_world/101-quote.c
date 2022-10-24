15a1e2001cab76cf2bfc7e8fcb4cdbc55b3b2b7d
Montseng <montseng.monaheng@gmail.com>
2022-10-24 08:31:34 -0700

    playingwithc

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - C program that prints followed by a new line 
+ * C program that prints and moves to the next line
  *
- * Return: 0 
- */
+ * Return: 0
+*/
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
